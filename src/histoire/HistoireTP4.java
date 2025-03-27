0222453ed6c09eeb24cb8731c0b259012234f36d
Ilan Dorso <drl5398a@ensfsi.onmicrosoft.com>
2025-03-27 12:01:42 +0100

    début création de la classe ronin

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -1,13 +1,12 @@
 package histoire;
 
 import personnages.Commercant;
+import personnages.Ronin;
 import personnages.Yakuza;
 
 public class HistoireTP4 {
 	public static void main(String[] args) {
 		Commercant marco = new Commercant("Marco", 20);
-		Yakuza yakuLeNoir = new Yakuza("Yaku Le Noir", "Whisky", 30, "Warsong");
-		yakuLeNoir.direBonjour();
-		yakuLeNoir.extorquer(marco);
+		Ronin roro = ne
 	}
 }
---
+++ src/personnages/Ronin.java
@@ -0,0 +1,15 @@
+package personnages;
+
+public class Ronin extends Humain{
+	public Ronin(String nom, String boissonFavorite, int argent) {
+		super(nom, boissonFavorite, argent);
+	}
+
+	private int honneur = 1;
+	
+	public void donner(Commercant beneficiaire) {
+		int don = argent/10;
+		parler(beneficiaire + " prend ces " + don + " sous.");
+		beneficiaire.gagnerArgent(don);
+	}
+}
