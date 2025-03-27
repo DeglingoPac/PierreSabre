422a26032725bb05d9f2fd9ebf86d697e3e65acc
Ilan Dorso <drl5398a@ensfsi.onmicrosoft.com>
2025-03-27 11:35:52 +0100

    création de la classe commercant

---
+++ src/personnages/Commercant.java
@@ -0,0 +1,21 @@
+package personnages;
+
+public class Commercant extends Humain{
+	private static final String boissonFavorite = "thé";
+	
+	public Commercant(String nom, int argent) {
+		super(nom, boissonFavorite, argent);
+	}
+
+	public int seFaireExtorquer() {
+		parler("J'ai tout perdu ! Le monde est trop injuste...");
+		int sommePerdue = argent;
+		perdreArgent(sommePerdue);
+		return sommePerdue;
+	}
+	
+	public void recevoir(int argent) {
+		gagnerArgent(argent);
+		parler(argent + " sous ! Je te remercie généreux donnateur !");
+	}
+}
